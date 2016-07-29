af2fa3874656965869f44ca7471d29cdfbf5a534
rav bay <ravzski@gmail.com>
2016-07-29 18:49:22 +0800

    added speech creation

--- app/controllers/api/v1/speeches_controller.rb
+++ app/controllers/api/v1/speeches_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::SpeechesController < ApiController
     puts "*********************"
     puts params[:file]
     puts params[:title]
+    Speech.create(title: params[:title])
     puts "*********************"
     render json: {text: Wit::Client.new.post(params[:file])}
   end
